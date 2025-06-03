9436bb15a16426fe6b483054e155d437691815b4
Shivani Jha <jhashivaniradhekrishna@gmail.com>
2025-06-03 15:03:16 +0000

    ==> Publish directory Build does not exist! ==> Build failed 😞

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone', // Add this for a self-contained build
   typescript: {
     ignoreBuildErrors: true,
   },
